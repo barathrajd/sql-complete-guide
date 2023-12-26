79b24266888be9ce64c280f019c1b1ae823b46a7
Barathraj <barathdeva757@gmail.com>
2023-12-26 19:57:59 +0530

    insert the data

---
+++ 03-insert-user-data.sql
@@ -0,0 +1,12 @@
+-- INSERT INTO users (
+--         full_name,
+--         yearly_salary,
+--         current_status
+--     )
+-- VALUES ('Barathraj', 6000000, 'employed')
+INSERT INTO users (
+        full_name,
+        yearly_salary,
+        current_status
+    )
+VALUES ('Raj', 6000000, 'employed')
